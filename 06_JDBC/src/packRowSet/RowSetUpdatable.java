d07f0b83a5422e7a68c79e6ea833796842ae0805
mishrasm2022@gmail.com <239933501+Sudeeppp-Mishra@users.noreply.github.com>
2025-07-15 06:03:05 +0545

    update: issue of not updating resolved

--- 06_JDBC/src/packRowSet/RowSetUpdatable.java
+++ 06_JDBC/src/packRowSet/RowSetUpdatable.java
@@ -6,10 +6,11 @@ package packRowSet;
 import javax.sql.rowset.JdbcRowSet;
 import javax.sql.rowset.RowSetFactory;
 import javax.sql.rowset.RowSetProvider;
+import java.sql.ResultSet;
 
 public class RowSetUpdatable {
     public static void main(String[] args) throws Exception {
-        // Create RowSetFactory (preferred over using JdbcRowSetImpl directly)
+        // Create RowSetFactory
         RowSetFactory factory = RowSetProvider.newFactory();
 
         // Create JdbcRowSet using the factory
@@ -18,39 +19,45 @@ public class RowSetUpdatable {
         // Set DB connection details
         rowSet.setUrl("jdbc:mysql://localhost:3306/TestPractice");
         rowSet.setUsername("root");
-        rowSet.setPassword("");  // Set your DB password here if needed
+        rowSet.setPassword("");  // Set your DB password here
 
-        // Set SQL SELECT command (the rowset will be scrollable and updatable)
+        // IMPORTANT: Set scrollability and updatability
+        rowSet.setType(ResultSet.TYPE_SCROLL_INSENSITIVE);
+        rowSet.setConcurrency(ResultSet.CONCUR_UPDATABLE);
+
+        // Set SQL command and execute
         rowSet.setCommand("SELECT * FROM test");
-        rowSet.execute(); // Executes the query and fills rowSet with the data
+        rowSet.execute();
 
         // Iterate and update the matching record
         while (rowSet.next()) {
             if (rowSet.getInt("roll") == 1) {
-                // Update the "name" column
                 rowSet.updateString("name", "Updated Name");
-
-                // Commit the update to the database
-                rowSet.updateRow();
-
+                rowSet.updateRow(); // Commit changes to DB
                 System.out.println("Successfully updated record with roll = 1");
             }
         }
 
-        //  Close the rowSet
+        // Close the rowSet
         rowSet.close();
     }
 }
 
 /*
 NOTES:
- What makes it Updatable?
-- You can use methods like:
-    - updateString(columnName, value)
-    - updateInt(columnName, value)
-    - updateRow() → to commit those changes to DB
 
-Requirements:
-- Add `rs2xml.jar` or required rowset JARs if you face class loading issues.
-- Your database user must have UPDATE privileges on the table.
+✔ What makes it Updatable?
+- `setConcurrency(ResultSet.CONCUR_UPDATABLE)`
+- `setType(ResultSet.TYPE_SCROLL_INSENSITIVE)` (required for updatability)
+- Methods used:
+    • updateString(column, value)
+    • updateRow()
+
+ Requirements:
+- Ensure your MySQL user has UPDATE privileges
+- Don't forget to call `updateRow()` or changes won’t apply!
+- Make sure the underlying SQL SELECT includes a **primary key** or updatable result
+
+ Without setting concurrency, you'll get:
+   java.sql.SQLException: ResultSet is not updatable
 */
